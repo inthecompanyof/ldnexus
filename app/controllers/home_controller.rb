796869e8473b31c5dbc0c8cf751a7f7eb8df5d49
Marcin Stecki <marcin@netguru.pl>
2014-02-20 09:40:17 +0100

    some work on hompage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
-  expose(:recent_supports){ Support.limit(20).done.includes(:user, :receiver, :topic) }
-  expose(:waiting_for_help){ Support.limit(20).not_done.includes(:user, :receiver, :topic) }
+  expose(:recent_supports){
+    SupportDecorator.decorate_collection Support.limit(20).done.includes(:user, :receiver, :topic)
+  }
+  expose(:waiting_for_help){
+    SupportDecorator.decorate_collection Support.limit(20).not_done.includes(:user, :receiver, :topic)
+  }
 
 end
