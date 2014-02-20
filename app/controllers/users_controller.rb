f26ec65017ef67bbe0baf61bbcc6040665ba486d
Marcin Stecki <marcin@netguru.pl>
2014-02-20 21:47:03 +0100

    minor twaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,5 @@ class UsersController < ApplicationController
   expose_decorated(:users){ User.order(supports_count: :desc) }
   expose_decorated(:user){ User.find params[:id] }
   expose_decorated(:supports){ user.supports.done }
+  expose_decorated(:skills){ user.skills.includes(:topic) }
 end
--- app/decorators/skill_decorator.rb
+++ app/decorators/skill_decorator.rb
@@ -1,4 +1,8 @@
 class SkillDecorator < Draper::Decorator
   decorates :skill
 
+  def to_s
+    skill.topic.to_s
+  end
+
 end
