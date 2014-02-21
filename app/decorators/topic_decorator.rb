fa4d229310ffcd3a92a2a66dcf97646dada2d71d
Marcin Stecki <marcin@netguru.pl>
2014-02-21 12:37:56 +0100

    all supproters, session

--- app/decorators/topic_decorator.rb
+++ app/decorators/topic_decorator.rb
@@ -9,4 +9,8 @@ class TopicDecorator < Draper::Decorator
   def small_title
     topic.to_s.downcase
   end
+
+  def supporters
+    h.raw topic.users.map{|user| h.link_to(user, user) }.join(", ")
+  end
 end
