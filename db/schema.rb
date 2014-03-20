401d8484db71f767900fe628532195ad626ebdfa
Adam Nowak <adam.nowak@netguru.pl>
2014-03-20 22:04:36 +0100

    add migration for state in supports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140312032352) do
+ActiveRecord::Schema.define(version: 20140320194439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140312032352) do
     t.integer  "receiver_id"
     t.boolean  "done",        default: false
     t.text     "body"
+    t.string   "state",       default: "pending"
   end
 
   add_index "supports", ["receiver_id"], name: "index_supports_on_receiver_id", using: :btree
