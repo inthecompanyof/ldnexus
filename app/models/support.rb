23eb6b4920300266a7e50605d75b62e9aa36e5f8
Adam Nowak <adam.nowak@netguru.pl>
2014-03-20 22:06:35 +0100

    add state machine logic

--- app/models/support.rb
+++ app/models/support.rb
@@ -9,4 +9,23 @@ class Support < ActiveRecord::Base
   scope :not_done, -> { where(done: false) }
   default_scope -> { order(created_at: :desc) }
 
+  state_machine initial: :pending do
+    event :ack do
+      transition pending: :acknowledged
+    end
+    after_transition on: :ack, do: :send_ack_mailing
+
+    event :finish do
+      transition [:pending, :acknowledged] => :done
+    end
+    after_transition on: :finish, do: :send_finish_mailing
+  end
+
+  def send_ack_mailing
+    # TODO: send emails here
+  end
+
+  def send_finish_mailing
+    # TODO: send emails here
+  end
 end
