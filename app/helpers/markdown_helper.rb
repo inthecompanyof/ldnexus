f0f4464bf36420cb35868d213d4a6615e4f342f0
Marcin Brzeziński <cintrzyk@gmail.com>
2014-03-21 09:02:51 +0100

    markdown helper method

---
+++ app/helpers/markdown_helper.rb
@@ -0,0 +1,31 @@
+module MarkdownHelper
+  class PygmentsRenderHTML < Redcarpet::Render::HTML
+    def block_code(source, language)
+      Pygments.highlight source, lexer: language, options: { linenos: true }
+    end
+  end
+
+  def markdown(content)
+    redcarpet_markdown.render(content).html_safe
+  end
+
+  def redcarpet_markdown
+    @redcarpet_markdown ||= Redcarpet::Markdown.new renderer, extensions
+  end
+
+  def extensions
+    {
+      autolink: true,
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      strikethrough: true,
+      lax_spacing: true,
+      disable_indented_code_blocks: true
+    }
+  end
+
+  def renderer
+    Redcarpet::Render::HTML.new no_styles: true, hard_wrap: true
+    PygmentsRenderHTML.new hard_wrap: true, filter_html: true, safe_links_only: true
+  end
+end
