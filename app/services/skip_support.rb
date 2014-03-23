ac65eec75044071244e70f533e4b6486dbb6b05b
Adam Nowak <adam.nowak@netguru.pl>
2014-03-23 16:22:32 +0100

    success != can_skip?

--- app/services/skip_support.rb
+++ app/services/skip_support.rb
@@ -1,28 +1,33 @@
 class SkipSupport < Struct.new(:support)
   require 'active_support/core_ext/string'
 
-  attr_accessor :candidate
+  attr_accessor :candidate, :success
   attr_writer :candidates
 
   def commence!
+    self.success = false
     self.candidate = next_candidate
     if can_skip?
       support.user = candidate
       support.save!
+      self.success = true
     end
     self
   end
 
   def success?
-    candidate.present?
+    success
   end
-  alias_method :can_skip?, :success?
 
   def candidates
     @candidates ||= support.topic.users.without support.user
   end
 
   private
+
+  def can_skip?
+    candidate.present?
+  end
 
   def next_candidate
     candidates.sample
