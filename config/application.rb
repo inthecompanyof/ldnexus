ee4383ddd4693b4aec9695fafa3503311d054350
Adam Nowak <adam.nowak@netguru.pl>
2014-04-13 12:47:37 +0200

    fix wrong order of requires

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
-require_relative 'preinitializer'
 require 'rails/all'
+require_relative 'preinitializer'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
