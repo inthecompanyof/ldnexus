cbd9e31794f36bf15d01cb4f685d567b43746503
Adam Nowak <lubieniebieski@gmail.com>
2014-04-16 18:08:11 +0200

    Update gravatar_helper.rb

--- app/helpers/gravatar_helper.rb
+++ app/helpers/gravatar_helper.rb
@@ -3,7 +3,7 @@ module GravatarHelper
   def gravatar_image_tag email, size
     hash = Digest::MD5.hexdigest(email)
     image_tag "//www.gravatar.com/avatar/#{hash}?s=#{size}",
-    height: size, width: size, class: 'user_avatar'
+              height: size, width: size, class: 'user_avatar'
   end
 
 end
