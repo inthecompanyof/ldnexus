22f03de954cb0cc7e34b43ca8673f57726e6f066
Dawid Woźniak <dawid.wozniak@netguru.pl>
2014-04-21 22:10:51 +0200

    single-quoted strings

--- app/decorators/support_decorator.rb
+++ app/decorators/support_decorator.rb
@@ -40,7 +40,7 @@ class SupportDecorator < Draper::Decorator
   def skip_button
     return if done? || support.user != h.current_user
 
-    h.link_to h.raw("Skip"), h.skip_support_path(object),
+    h.link_to h.raw('Skip'), h.skip_support_path(object),
               method: :post,
               confirm: "Do you really don't have time for this one?"
   end
@@ -60,13 +60,13 @@ class SupportDecorator < Draper::Decorator
   private
 
   def finish_button
-    h.link_to h.raw("Mark as resolved"), h.finish_support_path(object),
+    h.link_to h.raw('Mark as resolved'), h.finish_support_path(object),
       method: :post, class: 'btn btn-success',
       confirm: 'Are you sure you are done helping? This action will also set you as a supporter for this issue.'
   end
 
   def ack_button
-    h.link_to h.raw("Acknowledge!"), h.ack_support_path(object),
+    h.link_to h.raw('Acknowledge!'), h.ack_support_path(object),
       method: :post, class: 'btn btn-success'
   end
 end
