4206f424a817fe872421e96012760da7afd534f4
Adam Nowak <adam.nowak@netguru.pl>
2014-04-23 15:02:41 +0200

    remove empty line

--- app/models/support.rb
+++ app/models/support.rb
@@ -5,7 +5,6 @@ class Support < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic, counter_cache: true
 
-
   scope :done, -> { where(done: true) }
   scope :not_done, -> { where(done: false) }
   default_scope -> { order(created_at: :desc) }
