0f3788ea9d4784242a58742610b96ccd494a3644
Adam Nowak <adam.nowak@netguru.pl>
2014-04-27 21:31:29 +0200

    Prefer single-quoted strings when you don't need string interpolation or special symbols.

--- spec/services/finish_support_spec.rb
+++ spec/services/finish_support_spec.rb
@@ -5,13 +5,13 @@ describe FinishSupport do
   let(:support) { Support.new }
   subject { described_class.new user, support }
 
-  describe "commence!" do
+  describe 'commence!' do
 
     let(:another_user) { User.new }
 
     before { allow(support).to receive(:save!) }
 
-    describe "reassigns user support" do
+    describe 'reassigns user support' do
       context 'to user finishing the support' do
         it 'when user is some other user than the one requesting help' do
           support.user = another_user
@@ -29,12 +29,12 @@ describe FinishSupport do
       end
     end
 
-    it "finishes support" do
+    it 'finishes support' do
       subject.commence!
       expect(support).to be_done
     end
 
-    it "updates user counter" do
+    it 'updates user counter' do
       expect{ subject.commence! }.to change{ user.supports_count }.by(1)
     end
 
