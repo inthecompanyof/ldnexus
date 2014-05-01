2d039496c1332318d78b90fbfbc4bdaded72857f
arturro <artchru@gmail.com>
2014-05-01 08:25:50 +0200

    add specs

--- spec/services/comment_on_support_spec.rb
+++ spec/services/comment_on_support_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe CommentOnSupport do
-  subject { described_class.new(User.new, Support.new, {}) }
+  let(:user) { User.new }
+  subject { described_class.new(user, Support.new, {}) }
 
   describe "#commence!" do
     before { allow(subject).to receive(:deliver_email) }
@@ -15,6 +16,24 @@ describe CommentOnSupport do
       allow(subject.new_comment).to receive(:save!)
       expect(subject).to receive(:deliver_email)
       subject.commence!
+    end
+  end
+
+  describe "#subscribers" do
+    before {
+      subject.support.stub_chain(:comments, :pluck).and_return([11, 12, 13, user.id])
+    }
+    after { subject.subscribers }
+
+    it "returns all related users without commenting one"  do
+      expect(User).to receive(:where).with(id: [11, 12, 13])
+    end
+
+    context "commenting user is receiver of support" do
+      it "returns all related users without receiver of support" do
+        subject.stub(receiver_id: user.id) 
+        expect(User).to receive(where).with(id: [11, 12, 13])
+      end
     end
   end
 end
