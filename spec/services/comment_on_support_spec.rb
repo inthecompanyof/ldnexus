f9b7d6933a0442f8f621d5c3a0adf5a72c7dc07c
arturro <artchru@gmail.com>
2014-05-05 12:35:54 +0200

    hound fixes, rspec 3 syntax

--- spec/services/comment_on_support_spec.rb
+++ spec/services/comment_on_support_spec.rb
@@ -4,35 +4,35 @@ describe CommentOnSupport do
   let(:user) { User.new }
   subject { described_class.new(user, Support.new, {}) }
 
-  describe "#commence!" do
+  describe '#commence!' do
     before { allow(subject).to receive(:deliver_email) }
 
-    it "saves new comment" do
+    it 'saves new comment' do
       expect(subject.new_comment).to receive(:save!)
       subject.commence!
     end
 
-    it "sends email to subscribers" do
+    it 'sends email to subscribers' do
       allow(subject.new_comment).to receive(:save!)
       expect(subject).to receive(:deliver_email)
       subject.commence!
     end
   end
 
-  describe "#subscribers" do
-    before {
-      subject.support.stub_chain(:comments, :pluck)
-                      .and_return([11, 12, 13, user.id])
-    }
+  describe '#subscribers' do
+    before do
+      allow(subject.support).to receive_message_chain(:comments, :pluck).
+                                and_return([11, 12, 13, user.id])
+    end
     after { subject.subscribers }
 
-    it "returns all related users without commenting one"  do
+    it 'returns all related users without commenting one'  do
       expect(User).to receive(:where).with(id: [11, 12, 13])
     end
 
-    context "commenting user is receiver of support" do
-      it "returns all related users without receiver of support" do
-        subject.stub(receiver_id: user.id) 
+    context 'commenting user is receiver of support' do
+      it 'returns all related users without receiver of support' do
+        allow(subject).to receive(:receiver_id).and_return(user.id)
         expect(User).to receive(:where).with(id: [11, 12, 13])
       end
     end
